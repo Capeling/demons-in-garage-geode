592469d996d3ad2a1fe6370fd191a05b95973a18
Capeling <calebavincent@gmail.com>
2023-11-02 18:20:22 +1030

    mod code

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/GJGarageLayer.hpp>
+#include <UIBuilder.hpp>
 
 using namespace geode::prelude;
 
-class $modify(MenuLayer) {
-	void onMoreGames(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show(); 
-	} 
+class $modify(GJGarageLayer) {
+	bool init() {
+		if(!GJGarageLayer::init())
+			return false;
+		auto winSize = CCDirector::sharedDirector()->getWinSize();
+		CCDictionary* stats = GameStatsManager::sharedState()->m_playerStats;
+		int demonAmount = stats->valueForKey("5")->intValue();
+
+		auto demonIcon = Build<CCSprite>::createSpriteName("diffIcon_06_btn_001.png")
+			.parent(this)
+			.scale({0.525})
+			.anchorPoint({0.5, 0.5})
+			.pos({winSize.width - 20, winSize.height - 114});
+			
+		auto demonLabel = Build<CCLabelBMFont>::create(std::to_string(demonAmount).c_str(), "bigFont.fnt")
+			.pos({winSize.width - 36, winSize.height - 114})
+			.anchorPoint({1, 0.5})
+			.parent(this)
+			.scale(0.45);
+
+		return true;
+
+	}
 };
